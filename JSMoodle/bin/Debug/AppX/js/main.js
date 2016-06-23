e24159ca383679d5433bd5daf5e999bb80da1d39
Projeto Moodle CNAT <projetomoodlecnat@gmail.com>
2016-06-23 12:05:07 -0300

    23/06/2016. Tarefas de persistência de tabela, importação de tabela e seleção de queries habilitadas. Tratamento de erro para todas as novas funcionalidades também foram desenvolvidos.

--- JSMoodle/bin/Debug/AppX/js/main.js
+++ JSMoodle/bin/Debug/AppX/js/main.js
@@ -1,11 +1,15 @@
 ﻿$(document).ready(function () {
-    exportTable("teste");
 
-    $('#btn-hi').click(function () {
-        var statusParagraph = document.getElementById("status");
-        var url = "http://localhost:37006/api/DBProperties?index=" + document.getElementById("conIndex").value;
-        statusParagraph.setAttribute("class", "center light-green-text");
-        statusParagraph.innerHTML = "Requisição das informações do banco de dados em andamento...";
+    var awaiter = "null";
+    var lastLoadedTable = "";
+
+    //document.getElementById("btn-recuperar").onclick = getTableInJSONFromAPI();
+
+    $('#btn-recuperar').click(function () {
+        var conIndex = document.getElementById("conIndex").value.split(":")[0];
+        var queryIndex = document.getElementById("conIndex").value.split(":")[1];
+        var url = "http://localhost:37006/api/DBProperties?index=" + conIndex;
+        setStatus("progressing", "Requisição das informações do banco de dados em andamento...");
 
         $.ajax({
             type: "GET",
@@ -13,27 +17,25 @@
             async: true,
             contentType: "text",
             success: function (firstStep) {
-                statusParagraph.innerHTML = "Requisição dos registros das tabelas em andamento...";
+                setStatus("progressing", "Requisição dos registros das tabelas em andamento...");
                 try {
                     var results = JSON.parse(firstStep);
                     url = "http://localhost:37006/api/selector" + results[0]["databaseType"];
+                    lastLoadedTable = results[parseInt(queryIndex)]["tabela"];
                     // results[0] é o tipo do banco de dados
                     // results[n+1] é o seletor da query
-                    $.post((url), { 'connectionIndex': document.getElementById("conIndex").value, 'query': results[1]["comando"] }, function (data) {
+                    $.post((url), { 'connectionIndex': conIndex, 'query': results[parseInt(queryIndex)]["comando"] }, function (data) {
                         parseToTable(data);
                     }, "json");
                 } catch (exception) {
-                    statusParagraph.setAttribute("class", "center red-text");
-                    statusParagraph.innerHTML = "Requisição completou, mas a transformação da stream em JSON falhou.";
+                    setStatus("error", "Requisição completou, mas a transformação da stream em JSON falhou.");
                 }
-                statusParagraph.innerHTML = "Requisição dos registros da tabela completa. Aguarde enquanto transformo os resultados na tabela...";
+                //setStatus("progressing", "Requisição dos registros da tabela completa. Aguarde enquanto transformo os resultados na tabela...");
             },
         }).fail(function (jqXHR, textStatus) {
-            statusParagraph.setAttribute("class", "center red-text");
-            statusParagraph.innerHTML = "Requisição de aquisição das informações do banco de dados falhou.";
+            setStatus("error", "Requisição de aquisição das informações do banco de dados falhou.");
             console.log(jqXHR);
         });
-
     });
 
     function parseToTable(sender) {
@@ -51,10 +53,10 @@
             // Pondo nome nas colunas
             var i = 0;
             while (i < sender[0].length) {
-                strInnerHtml+="<th data-field='"+sender[0][i].toString()+"'>"+sender[0][i].toString()+"</th>";
+                strInnerHtml += "<th data-field='" + sender[0][i].toString() + "'>" + sender[0][i].toString() + "</th>";
                 i++;
             }
-            strInnerHtml+="</tr></thead>";
+            strInnerHtml += "</tr></thead>";
             contentTable.innerHTML = strInnerHtml;
 
             // Colocando os valores do SELECT nas colunas
@@ -69,48 +71,138 @@
             });
             contentTable.innerHTML += "</tbody>";
             contentTable.firstChild.nextSibling.removeChild(contentTable.firstChild.nextSibling.firstChild);
-            document.getElementById("status").setAttribute("class", "center blue-text");
-            document.getElementById("status").innerHTML = "Requisição finalizada com sucesso e todos os resultados foram carregados na tabela.";
-            document.getElementById("operacoesTabela").innerHTML += "<input type='button' value='Importar' class='btn-flat' /><input type='button' value='Exportar' class='btn-flat' />";
+            setStatus("succeeded", "Requisição finalizada com sucesso e todos os resultados foram carregados na tabela.");
+
+            // Adiciona os botões e reseta o estado do elemento ao recriá-lo
+            document.getElementById("operacoesTabela").innerHTML = "<input class='btn-flat' id='btn-recuperar' type='button' value='Recuperar a tabela'/><input id='btn-import' type='button' value='Importar' class='btn-flat' /><input id='btn-export' type='button' value='Exportar' class='btn-flat' />";
+            $("#btn-import").click(function () {
+                try {
+                    awaiter = "null";
+                    importTable(lastLoadedTable);
+                    var interval = setInterval(function () {
+                        if (awaiter != "null") {
+                            document.getElementById("table").innerHTML = awaiter;
+                            setStatus("succeeded", "A tabela foi importada com sucesso do arquivo e foi transformada numa tabela do HTML.");
+                            console.log("Interval cleared!");
+                            clearInterval(interval);
+                        }
+                    }, 2000);
+                } catch (exception) {
+                    console.log(exception);
+                }
+            });
+            $("#btn-export").click(function () {
+                try {
+                    exportTable(lastLoadedTable);
+                } catch (exception) {
+                    console.log(exception);
+                }
+            });
+            $('#btn-recuperar').click(function () {
+                var conIndex = document.getElementById("conIndex").value.split(":")[0];
+                var queryIndex = document.getElementById("conIndex").value.split(":")[1];
+                var url = "http://localhost:37006/api/DBProperties?index=" + conIndex;
+                setStatus("progressing", "Requisição das informações do banco de dados em andamento...");
+
+                $.ajax({
+                    type: "GET",
+                    url: url,
+                    async: true,
+                    contentType: "text",
+                    success: function (firstStep) {
+                        setStatus("progressing", "Requisição dos registros das tabelas em andamento...");
+                        try {
+                            var results = JSON.parse(firstStep);
+                            url = "http://localhost:37006/api/selector" + results[0]["databaseType"];
+                            lastLoadedTable = results[parseInt(queryIndex)]["tabela"];
+                            // results[0] é o tipo do banco de dados
+                            // results[n+1] é o seletor da query
+                            $.post((url), { 'connectionIndex': conIndex, 'query': results[parseInt(queryIndex)]["comando"] }, function (data) {
+                                parseToTable(data);
+                            }, "json");
+                        } catch (exception) {
+                            setStatus("error", "Requisição completou, mas a transformação da stream em JSON falhou.");
+                        }
+                        //setStatus("progressing", "Requisição dos registros da tabela completa. Aguarde enquanto transformo os resultados na tabela...");
+                    },
+                }).fail(function (jqXHR, textStatus) {
+                    setStatus("error", "Requisição de aquisição das informações do banco de dados falhou.");
+                    console.log(jqXHR);
+                });
+            });
         } catch (exception) {
-            document.getElementById("tableContents").innerHTML = "Transformação do objeto JSON em tabela falhou.";
+            setStatus("error", "Transformação do objeto JSON em tabela falhou.");
         }
     }
 
     function importTable(tableNameFile) {
-        var statusParagraph = document.getElementById("status");
+        console.log(tableNameFile);
         var tablePersisted = "";
-        statusParagraph.setAttribute("class", "center light-green-text");
-        statusParagraph.innerHTML = "Começando a importar dados do arquivo persistido...";
+        setStatus("progressing", "Começando a importar dados do arquivo persistido...");
         var filePicker = Windows.Storage.ApplicationData.current.localFolder.getFileAsync(tableNameFile);
         filePicker.done(function () {
             var fileReader = Windows.Storage.FileIO.readTextAsync(filePicker.operation.getResults());
             fileReader.done(function () {
-                tablePersisted = fileReader.operation.getResults().toString();
-                statusParagraph.innerHTML = "Dados carregados do arquivo. Transformando em tabela...";
-                return tablePersisted;
+                setStatus("progressing", "Dados carregados do arquivo. Transformando em tabela...");
+                console.log(tablePersisted);
+                awaiter = fileReader.operation.getResults().toString();
             }, function () {
-                statusParagraph.setAttribute("class", "center red-text");
-                statusParagraph.innerHTML = "Erro ao ler o arquivo. Verifique se você tem permissão de leitura ou se o arquivo permite leitura!";
-                return "error";
+                setStatus("error", "Erro ao ler o arquivo. Verifique se você tem permissão de leitura ou se o arquivo permite leitura!");
+                awaiter = "error";
             });
         }, function () {
-            statusParagraph.setAttribute("class", "center red-text");
-            statusParagraph.innerHTML = "Erro ao carregar o arquivo do computador. Verifique se ele existe!";
-            return "error";
+            setStatus("error", "Erro ao carregar o arquivo do computador. Verifique se ele existe!");
+            awaiter = "error";
         });
     }
 
     function exportTable(tableNameFile) {
-        var statusParagraph = document.getElementById("status");
-        statusParagraph.setAttribute("class", "center light-green-text");
-        statusParagraph.innerHTML = "Começando a persistir os dados da tabela no dispositivo...";
-        $.getScript("js/jquery.tabletojson.js").done(function () {
-            var table = $('#table').tableToJSON();
-            console.log(table);
-        }).fail(function () {
-
+        setStatus("progressing", "Começando a persistir os dados da tabela no dispositivo...");
+        var createFile = Windows.Storage.ApplicationData.current.localFolder.createFileAsync(tableNameFile);
+        createFile.done(function () {
+            setStatus("progressing", "Arquivo não existia e foi criado. Aguarde enquanto persisto a tabela...");
+            var writer = Windows.Storage.FileIO.writeTextAsync(createFile.operation.getResults(),  tableInnerHTML(document.getElementById("table")));
+            writer.done(function () {
+                setStatus("succeeded", "Tabela persistida com sucesso. Você pode tentar carregá-la utilizando o botão de importação.");
+            }, function () {
+                setStatus("error", "Não foi possível escrever no arquivo criado.");
+            });
+        }, function (failure) {
+            if (failure.toString().indexOf("existente")>-1) {
+                setStatus("progressing", "Arquivo já existia. Aguarde enquanto eu o sobrescrevo!");
+                var filePicker = Windows.Storage.ApplicationData.current.localFolder.getFileAsync(tableNameFile);
+                filePicker.done(function () {
+                    setStatus("progressing", "Sobrescrevendo o arquivo com o conteúdo da tabela. Aguarde...")
+                    var writer = Windows.Storage.FileIO.writeTextAsync(filePicker.operation.getResults(), tableInnerHTML(document.getElementById("table")));
+                    writer.done(function () {
+                        setStatus("succeeded", "Tabela " + lastLoadedTable + " persistida com sucesso num arquivo de mesmo nome dessa.")
+                    }, function () {
+                        setStatus("error", "Não foi possível sobrescrever o arquivo.");
+                    });
+                }, function () {
+                    setStatus("error", "Não foi possível abrir o processo para sobrescrição do arquivo, verifique suas permissões de usuário.");
+                });
+            } else {
+                setStatus("error", "Ocorreu um erro sem tratamento pelo programador da aplicação. Contate os desenvolvedores.");
+            }
         });
+    }
+
+    function setStatus(type, message) {
+        if (type == "progressing") {
+            document.getElementById("status").setAttribute("class", "center light-green-text");
+            document.getElementById("status").innerHTML = message;
+        } else if (type == "error") {
+            document.getElementById("status").setAttribute("class", "center red-text");
+            document.getElementById("status").innerHTML = message;
+        } else {
+            document.getElementById("status").setAttribute("class", "center blue-text");
+            document.getElementById("status").innerHTML = message;
+        }
+    }
+
+    function tableInnerHTML(tableDomObject) {
+        return tableDomObject.innerHTML;
     }
     //end
 });
