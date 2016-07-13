713496727e5efdeec46b5bb12d5379e9facc1cb9
Projeto Moodle CNAT <projetomoodlecnat@gmail.com>
2016-07-13 14:59:18 -0300

    13/07/2016. A versão contém a autenticação pelo site feito de forma mais consistente, quase que garantida de funcionar para qualquer tipo de página de login de Moodle que tenha como alvo. Também está incluído o redirecionamento para (o que se tornará) a página de Dashboard da aplicação, onde o usuário autenticado poderá escolher as tarefas a serem respondidas e ter uma visão geral do Moodle.

--- JSMoodle/js/main.js
+++ JSMoodle/js/main.js
@@ -1,6 +1,8 @@
 ﻿$(document).ready(function () {
 
-    $('#login').focus(function () {
+    // Definindo os listeners padrão de elementos da interface
+
+    $('#username').focus(function () {
         document.getElementById("tooltipLabel").setAttribute("class", "red-text");
         document.getElementById("loginSpan").setAttribute("class", "bold blue-text");
     }).blur(function () {
@@ -9,7 +11,48 @@
     });
 
     // Método que realiza a autenticação pelo site do Moodle
-    
+
+    $("#btnSubmitLogin").click(function () {
+        $.ajax("http://localhost:37006/api/dbsites?databaseindex=" + document.getElementById("moodleSelector").value, {
+            contentType: "application/json",
+            method: "GET",
+            async: true,
+            success: function (firstStep) {
+                setStatus("progressing", "O site do Moodle está disponível e ativo. Tentando a autenticação... ");
+                jQuery.post({
+                    url: firstStep[0] + "login/index.php",
+                    data: { 'username': document.getElementById("username").value, 'password': document.getElementById("password").value }
+                }).done(function (data, textStatus, jqXHR) {
+                    // A tag meta está presente somente na página de índice de login
+                    if (data.indexOf('meta name="robots"') == -1) {
+                        document.getElementById("username").setAttribute("disabled", "true");
+                        document.getElementById("password").setAttribute("disabled", "true");
+                        setStatus("succeeded", "A autenticação pelo site do Moodle foi concluída com sucesso!<br>Redirecionando para a página de Dashboard... ");
+                        setTimeout(function () {
+                            window.location = window.location.toString().substring(0, location.toString().lastIndexOf("/")) + "/dashboard/index.html";
+                        }, 2000);
+                        return;
+                    }
+                    setStatus("error", "O processo de autenticação foi concluído com sucesso, mas o usuário ou senha estão incorretos. ");
+                }).error(function () {
+                    setStatus("error", "A requisição de autenticação com a página de Login do Moodle falhou. ");
+                    return;
+                });
+            }, error: function () {
+                setStatus("error", "A requisição de autenticação com a página de Login do Moodle falhou. ");
+            }
+        }).error(function () {
+            setStatus("error", "A requisição de acesso à API do Moodle falhou. ");
+        });
+    });
+
+
+    // Método que realiza a autenticação através de seleção na tabela de usuários e um código hash codificador
+
+    //======================================A SER IMPLEMENTADO================================================
+
+    // Métodos que populam a interface com informações
+
     function setStatus(type, message) {
         if (type == "progressing") {
             document.getElementById("status").setAttribute("class", "center light-green-text");
@@ -22,220 +65,4 @@
             document.getElementById("status").innerHTML = message;
         }
     }
-
-    $("#btnSubmitLogin").click(function () {
-        $.ajax("http://localhost:37006/api/dbsites?databaseindex=" + document.getElementById("moodleSelector").value, {
-            contentType: "application/json",
-            method: "GET",
-            async: true,
-            success: function (firstStep) {
-                setStatus("progressing", "O site do Moodle está disponível e ativo. Tentando a autenticação... ");
-                jQuery.post(firstStep[0] + "login/index.php", { 'username': document.getElementById("username").value, 'password': document.getElementById("password").value },
-                    function (data) {
-                        if (data.indexOf("Log in to the site") == -1) {
-                            setStatus("success", "O usuário foi autenticado com sucesso! ");
-                        } else {
-                            setStatus("error", "O usuário ou senha estão incorretos e a autenticação falhou. ");
-                        }
-                    });
-            }, error: function () {
-                setStatus("error", "A requisição de autenticação com a página de Login do Moodle falhou");
-            }
-        })
-    });
-
-
-    // Método que realiza a autenticação através de seleção na tabela de usuários e um código hash codificador
-
-
-
-    // Recursos da DEMO
-
-    /*$('#btn-recuperar').click(function () {
-        var conIndex = document.getElementById("conIndex").value.split(":")[0];
-        var queryIndex = document.getElementById("conIndex").value.split(":")[1];
-        var url = "http://localhost:37006/api/DBProperties?index=" + conIndex;
-        setStatus("progressing", "Requisição das informações do banco de dados em andamento...");
-
-        $.ajax({
-            type: "GET",
-            url: url,
-            async: true,
-            contentType: "text",
-            success: function (firstStep) {
-                setStatus("progressing", "Requisição dos registros das tabelas em andamento...");
-                try {
-                    var results = JSON.parse(firstStep);
-                    url = "http://localhost:37006/api/selector" + results[0]["databaseType"];
-                    lastLoadedTable = results[parseInt(queryIndex)]["tabela"];
-                    // results[0] é o tipo do banco de dados
-                    // results[n+1] é o seletor da query
-                    $.post((url), { 'connectionIndex': conIndex, 'query': results[parseInt(queryIndex)]["comando"] }, function (data) {
-                        parseToTable(data);
-                    }, "json");
-                } catch (exception) {
-                    setStatus("error", "Requisição completou, mas a transformação da stream em JSON falhou.");
-                }
-                //setStatus("progressing", "Requisição dos registros da tabela completa. Aguarde enquanto transformo os resultados na tabela...");
-            },
-        }).fail(function (jqXHR, textStatus) {
-            setStatus("error", "Requisição de aquisição das informações do banco de dados falhou.");
-            console.log(jqXHR);
-        });
-    });
-
-    function parseToTable(sender) {
-        try {
-            var contentTable = document.getElementById("table");
-            if (sender[0].indexOf("Exception") > -1) {
-                contentTable.innerHTML = "";
-                contentTable.appendChild(document.createElement("tr"));
-                var lastChildOfTable = contentTable.lastChild;
-                lastChildOfTable.innerHTML += "<td>" + sender[0] + "</td>";
-                return;
-            }
-            var strInnerHtml = "<thead><tr>";
-
-            // Pondo nome nas colunas
-            var i = 0;
-            while (i < sender[0].length) {
-                strInnerHtml += "<th data-field='" + sender[0][i].toString() + "'>" + sender[0][i].toString() + "</th>";
-                i++;
-            }
-            strInnerHtml += "</tr></thead>";
-            contentTable.innerHTML = strInnerHtml;
-
-            // Colocando os valores do SELECT nas colunas
-            $(sender).each(function () {
-                contentTable.appendChild(document.createElement("tr"));
-                var lastChildOfTable = contentTable.lastChild;
-                var row = $(this);
-                row.each(function (row, td) {
-                    lastChildOfTable.innerHTML += "<td>" + td + "</td>";
-                });
-                return;
-            });
-            contentTable.innerHTML += "</tbody>";
-            contentTable.firstChild.nextSibling.removeChild(contentTable.firstChild.nextSibling.firstChild);
-            setStatus("succeeded", "Requisição finalizada com sucesso e todos os resultados foram carregados na tabela.");
-
-            // Adiciona os botões e reseta o estado do elemento ao recriá-lo
-            document.getElementById("operacoesTabela").innerHTML = "<input class='btn-flat' id='btn-recuperar' type='button' value='Recuperar a tabela'/><input id='btn-import' type='button' value='Importar' class='btn-flat' /><input id='btn-export' type='button' value='Exportar' class='btn-flat' />";
-            $("#btn-import").click(function () {
-                try {
-                    awaiter = "null";
-                    importTable(lastLoadedTable);
-                    var interval = setInterval(function () {
-                        if (awaiter != "null") {
-                            document.getElementById("table").innerHTML = awaiter;
-                            setStatus("succeeded", "A tabela foi importada com sucesso do arquivo e foi transformada numa tabela do HTML.");
-                            console.log("Interval cleared!");
-                            clearInterval(interval);
-                        }
-                    }, 2000);
-                } catch (exception) {
-                    console.log(exception);
-                }
-            });
-            $("#btn-export").click(function () {
-                try {
-                    exportTable(lastLoadedTable);
-                } catch (exception) {
-                    console.log(exception);
-                }
-            });
-            $('#btn-recuperar').click(function () {
-                var conIndex = document.getElementById("conIndex").value.split(":")[0];
-                var queryIndex = document.getElementById("conIndex").value.split(":")[1];
-                var url = "http://localhost:37006/api/DBProperties?index=" + conIndex;
-                setStatus("progressing", "Requisição das informações do banco de dados em andamento...");
-
-                $.ajax({
-                    type: "GET",
-                    url: url,
-                    async: true,
-                    contentType: "text",
-                    success: function (firstStep) {
-                        setStatus("progressing", "Requisição dos registros das tabelas em andamento...");
-                        try {
-                            var results = JSON.parse(firstStep);
-                            url = "http://localhost:37006/api/selector" + results[0]["databaseType"];
-                            lastLoadedTable = results[parseInt(queryIndex)]["tabela"];
-                            // results[0] é o tipo do banco de dados
-                            // results[n+1] é o seletor da query
-                            $.post((url), { 'connectionIndex': conIndex, 'query': results[parseInt(queryIndex)]["comando"] }, function (data) {
-                                parseToTable(data);
-                            }, "json");
-                        } catch (exception) {
-                            setStatus("error", "Requisição completou, mas a transformação da stream em JSON falhou.");
-                        }
-                        //setStatus("progressing", "Requisição dos registros da tabela completa. Aguarde enquanto transformo os resultados na tabela...");
-                    },
-                }).fail(function (jqXHR, textStatus) {
-                    setStatus("error", "Requisição de aquisição das informações do banco de dados falhou.");
-                    console.log(jqXHR);
-                });
-            });
-        } catch (exception) {
-            setStatus("error", "Transformação do objeto JSON em tabela falhou.");
-        }
-    }
-
-    function importTable(tableNameFile) {
-        console.log(tableNameFile);
-        var tablePersisted = "";
-        setStatus("progressing", "Começando a importar dados do arquivo persistido...");
-        var filePicker = Windows.Storage.ApplicationData.current.localFolder.getFileAsync(tableNameFile);
-        filePicker.done(function () {
-            var fileReader = Windows.Storage.FileIO.readTextAsync(filePicker.operation.getResults());
-            fileReader.done(function () {
-                setStatus("progressing", "Dados carregados do arquivo. Transformando em tabela...");
-                console.log(tablePersisted);
-                awaiter = fileReader.operation.getResults().toString();
-            }, function () {
-                setStatus("error", "Erro ao ler o arquivo. Verifique se você tem permissão de leitura ou se o arquivo permite leitura!");
-                awaiter = "error";
-            });
-        }, function () {
-            setStatus("error", "Erro ao carregar o arquivo do computador. Verifique se ele existe!");
-            awaiter = "error";
-        });
-    }
-
-    function exportTable(tableNameFile) {
-        setStatus("progressing", "Começando a persistir os dados da tabela no dispositivo...");
-        var createFile = Windows.Storage.ApplicationData.current.localFolder.createFileAsync(tableNameFile);
-        createFile.done(function () {
-            setStatus("progressing", "Arquivo não existia e foi criado. Aguarde enquanto persisto a tabela...");
-            var writer = Windows.Storage.FileIO.writeTextAsync(createFile.operation.getResults(),  tableInnerHTML(document.getElementById("table")));
-            writer.done(function () {
-                setStatus("succeeded", "Tabela persistida com sucesso. Você pode tentar carregá-la utilizando o botão de importação.");
-            }, function () {
-                setStatus("error", "Não foi possível escrever no arquivo criado.");
-            });
-        }, function (failure) {
-            if (failure.toString().indexOf("existente")>-1) {
-                setStatus("progressing", "Arquivo já existia. Aguarde enquanto eu o sobrescrevo!");
-                var filePicker = Windows.Storage.ApplicationData.current.localFolder.getFileAsync(tableNameFile);
-                filePicker.done(function () {
-                    setStatus("progressing", "Sobrescrevendo o arquivo com o conteúdo da tabela. Aguarde...")
-                    var writer = Windows.Storage.FileIO.writeTextAsync(filePicker.operation.getResults(), tableInnerHTML(document.getElementById("table")));
-                    writer.done(function () {
-                        setStatus("succeeded", "Tabela " + lastLoadedTable + " persistida com sucesso num arquivo de mesmo nome dessa.")
-                    }, function () {
-                        setStatus("error", "Não foi possível sobrescrever o arquivo.");
-                    });
-                }, function () {
-                    setStatus("error", "Não foi possível abrir o processo para sobrescrição do arquivo, verifique suas permissões de usuário.");
-                });
-            } else {
-                setStatus("error", "Ocorreu um erro sem tratamento pelo programador da aplicação. Contate os desenvolvedores.");
-            }
-        });
-    }
-
-    function tableInnerHTML(tableDomObject) {
-        return tableDomObject.innerHTML;
-    }
-    //end*/
 });
