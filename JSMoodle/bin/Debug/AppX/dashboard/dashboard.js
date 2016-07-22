f87e6f3bb8083d6e5887d2257de1130b1dccd408
Projeto Moodle CNAT <projetomoodlecnat@gmail.com>
2016-07-22 16:28:06 -0300

    22/07/2016. Listagem de cursos que o participante está inserido foi adicionada à interface de 'Dashboard' da aplicação. Página de Dashboard começou a ser estilizada.

--- JSMoodle/bin/Debug/AppX/dashboard/dashboard.js
+++ JSMoodle/bin/Debug/AppX/dashboard/dashboard.js
@@ -1,23 +1,52 @@
-﻿var databaseIndex = getCookieValue("databaseIndex");
-var userId = getCookieValue("userId");
+﻿var cookiesDict = cookiesToDict();
+//SELECT fullname, shortname, `mdl_enrol`.`id` FROM `mdl_user_enrolments` INNER JOIN `mdl_enrol` ON `mdl_user_enrolments`.`enrolid` = `mdl_enrol`.`id` INNER JOIN `mdl_course` ON `mdl_enrol`.`courseid` = `mdl_course`.`id` where userid=2
 
 $(document).ready(function () {
     document.getElementsByTagName("h3")[0].innerHTML += "<b>" + document.cookie.substring(document.cookie.lastIndexOf("=") + 1) + "</b>";
-    $.ajax("http://localhost:37006/api/dbproperties?index=" + databaseIndex, {
+    $.ajax("http://localhost:37006/api/dbproperties?index=" + (cookiesDict["databaseIndex"] - 1), {
         method: "GET",
+        async: true,
+        contentType: "application/json",
         success: function (firstStep) {
             try {
                 firstStep = JSON.parse(firstStep);
                 databaseIndex = firstStep[1]["idconexao"];
             } catch (Exception) {
                 //setStatus("error", "Parsing dos dados da API falhou no primeiro estágio.");
+                console.log("ERROR: Parsing dos dados da API falhou no primeiro estágio.");
                 return;
             }
             $.post({
                 url: "http://localhost:37006/api/selector" + firstStep[0]["databaseType"],
-                data: { "connectionIndex": databaseIndex - 1, "query": firstStep[4]["comando"] + " where userid='" + userId + "'" }
+                async: true,
+                data: { "connectionIndex": (cookiesDict["databaseIndex"] - 1), "query": firstStep[4]["comando"] + " where userid=" + cookiesDict["userId"] }
             }).done(function (data, textStatus, jqXHR) {
+                strBuilder = "";
+                var i = 1;
+                if (data.length == 1) {
+                    strBuilder = "<li>Você não está cadastrado em curso algum.</li>";
+                } else {
+                    for (; data[i] ;) {
+                        strBuilder += "<li class='bordered courseli'>• " + data[i][0] + " (<b tooltipvalue='   Sigla'>" + data[i][1] + "</b>) <i tooltipvalue='   Código do curso'>ID:" + data[i][2] + "</i>         <input type='button' class='btnExpand' value='+' /></li><br>";
+                        i++;
+                    }
+                }
+                document.getElementById("courses").innerHTML = strBuilder;
+                $('li > b').each(function () {
+                    $(this).hover(function () {
+                        showTooltip($(this));
+                    }, function () {
+                        deleteTooltip($(this));
+                    });
+                });
 
+                $('li > i').each(function () {
+                    $(this).hover(function () {
+                        showTooltip($(this));
+                    }, function () {
+                        deleteTooltip($(this));
+                    });
+                });
             });
         }
     })
@@ -26,6 +55,20 @@ $(document).ready(function () {
     });
 });
 
-function getCookieValue(key) {
-    return document.cookie.substring(document.cookie.indexOf(key) + key.length+1, document.cookie.substring(document.cookie.indexOf(key)).indexOf(";"));
+function cookiesToDict() {
+    var hashTable = {}
+    var i = 0;
+    for (; document.cookie.split(";")[i];) {
+        hashTable[document.cookie.split(";")[i].substring(0, document.cookie.split(";")[i].indexOf("=")).trim()] = document.cookie.split(";")[i].substring(document.cookie.split(";")[i].indexOf("=") + 1);
+        i++;
+    }
+    return hashTable;
+}
+
+function showTooltip(sender) {
+    sender.parent().append("<span class='inline-bubble'>" + sender[0].getAttribute("tooltipvalue") + "</span>");
+}
+
+function deleteTooltip(sender) {
+    sender[0].parentNode.removeChild(sender[0].parentNode.lastChild);
 }
