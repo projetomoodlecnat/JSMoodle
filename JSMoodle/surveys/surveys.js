dcbb4bc56bdc1884b7066d1980d511efbb0a495b
jpsouzasilva <jpgremio@gmail.com>
2016-11-23 13:14:17 -0300

    23/11/2016. Começo do suporte à verificação de campos e envio em formulários de enquetes.

--- JSMoodle/surveys/surveys.js
+++ JSMoodle/surveys/surveys.js
@@ -17,9 +17,12 @@ $(document).ready(function () {
     if (canUserAttemptSurvey_) {
         initializeFolder();
     } else {
-        if (canUserAttemptSurvey_ == "error") {
+        if (canUserAttemptSurvey_ == "network_error") {
             $("#content").html("Erro na requisição de checagem! Estarei tentando carregar os arquivos do dispositivo...");
             initializeFolder();
+            return;
+        } else if (canUserAttemptSurvey_ = "exception_error") {
+            $("#content").html("Erro de processamento na aplicação.");
             return;
         } else {
             $("#content").html("Você já completou essa enquete.");
@@ -30,19 +33,23 @@ $(document).ready(function () {
 
 function canUserAttemptSurvey() {
     var result;
-    $.post({
-        url: cookiesDict["api_Path"] + "selector" + cookiesDict["databaseType"],
-        async: false,
-        data: { "connectionIndex": cookiesDict["databaseIndex"], "query": "select * from mdl_feedback_completed where userid=" + cookiesDict["userID"] + "and feedback=" + feedbackID }
-    }).done(function (data, textStatus, jqXHR) {
-        if (data.length > 1) {
-            result = false;
-        } else {
-            result = true;
-        }
-    }).error(function () {
-        result = "error";
-    });
+    try {
+        $.post({
+            url: cookiesDict["api_Path"] + "selector" + cookiesDict["databaseType"],
+            async: false,
+            data: { "connectionIndex": cookiesDict["databaseIndex"], "query": "select multiple_submit from mdl_feedback_completed inner join mdl_feedback on feedback = mdl_feedback.id where userid=" + cookiesDict["userID"] + " and feedback=" + feedbackID + " limit 1" }
+        }).done(function (data, textStatus, jqXHR) {
+            if (data.length == 1 || data[1][data[0].indexOf("multiple_submit")] == "0") {
+                result = true;
+            } else {
+                result = false;
+            }
+        }).error(function () {
+            result = "network_error";
+        });
+    } catch (exception) {
+        result = "exception_error";
+    }
     return result;
 }
 
@@ -145,11 +152,99 @@ function loadListeners() {
         event.target.innerHTML = "<img src='../images/universal/loading.gif' />";
         saveToFile(surveyFile, document.body.outerHTML);
     });
+    loadFinishListener();
+}
+
+function requiredFieldCheck() {
+    var booleanToReturn = true;
+    $("[required=true]").each(function () {
+        var field = $(this);
+        if ((field.attr('type') == 'INPUT' && field.val() == "") || (field.attr('type') == 'DIV' && field.find('input[checked=true]').length == 0)) {
+            booleanToReturn = false;
+            Materialize.toast("Você precisa preencher alguns campos obrigatórios!", 1000);
+            return false;
+        }
+        if (field.attr('type') == 'number') {
+            if (parseFloat(field.val()) > parseFloat(field.attr('max')) || parseFloat(field.val()) < parseFloat(field.attr('min'))) {
+                booleanToReturn = false;
+                Materialize.toast("Você digitou uma resposta fora da zona de números permitida!", 1000);
+                return false;
+            }
+        } else if (field.attr('type') == 'text') {
+            if (parseFloat(field.val().length) > parseFloat(field.attr('maxlength'))) {
+                booleanToReturn = false;
+                Materialize.toast("Você digitou uma resposta maior do que a quantidade de caracteres permitida!", 1000);
+                return false;
+            }
+        }
+    });
+    return booleanToReturn;
 }
 
 function loadFinishListener() {
-    $('.paragraphFinishSurvey').click(function () {
+    $(".paragraphFinishSurvey").click(function () {
+        if (canUserAttemptSurvey() && requiredFieldCheck()) {
+            $(".paragraphFinishSurvey").remove();
+            processSurveyAttempt();
+        } else {
+            Materialize.toast("Erro na submissão! Verifique se você pode responder essa enquete.", 2000);
+        }
+    });
+}
 
+function processSurveyAttempt() {
+    $.post({
+        url: cookiesDict["api_Path"] + "selector" + cookiesDict["databaseType"],
+        data: { "connectionIndex": firstStep[1]["idconexao"] - 1, "query": 'select completed, anonymous from mdl_feedback_tracking where userid=' + cookiesDict["userID"] +
+        ' order by completed desc limit 1'}
+    }).done(function (data, textStatus, jqXHR) {
+        var howManyCompleted;
+        var anonymous;
+        if (data.length == 1) {
+            howManyCompleted = 0;
+        } else {
+            howManyCompleted = parseInt(data[1][data[0].indexOf("COMPLETED")]);
+            anonymous = data[1][data[0].indexOf("ANONYMOUS")];
+        }
+        $.post({
+            url: cookiesDict["api_Path"] + "selector" + cookiesDict["databaseType"],
+            data: {
+                "connectionIndex": firstStep[1]["idconexao"] - 1, "query": 'select id from mdl_feedback_tracking order by id desc limit 1' 
+            }
+        }).done(function (firstData, textStatus, jqXHR) {
+            var counter;
+            if (firstData.length == 1) {
+                var counter = 1;
+            } else {
+                var counter = parseInt(firstData[1][1])+1;
+            }
+            $.post({
+                url: cookiesDict["api_Path"] + "selector" + cookiesDict["databaseType"],
+                data: {
+                    "connectionIndex": firstStep[1]["idconexao"] - 1, "query": 'insert into mdl_feedback_completed(id,feedback,userid,timemodified,random_response,anonymous_response) values (' +
+                        counter.toString() + "," + feedbackID + "," + cookiesDict["userID"] + "," + getUnixTime() + ",0," + anonymous + ")"
+                }
+            }).done(function (secondData, textStatus, jqXHR) {
+                var tries = 1;
+                while (secondData.indexOf("error") > -1 && tries != 3) {
+                    counter++;
+                    $.post({
+                        url: cookiesDict["api_Path"] + "selector" + cookiesDict["databaseType"],
+                        async: false,
+                        data: {
+                            "connectionIndex": firstStep[1]["idconexao"] - 1, "query": 'insert into mdl_feedback_completed(id,feedback,userid,timemodified,random_response,anonymous_response) values (' +
+                                counter.toString() + "," + feedbackID + "," + cookiesDict["userID"] + "," + getUnixTime() + ",0," + anonymous + ")"
+                        }
+                    }).done(function (errorData, textStatus, jqXHR) {
+                        secondData = errorData;
+                    });
+                    tries++;
+                }
+                if (secondData.indexOf("error") > -1) {
+                    Materialize.toast("Erro SQL! Tente novamente mais tarde.", 3000);
+                }
+            });
+        });
     });
 }
 
@@ -182,17 +277,21 @@ function buildSurvey(surveyFile_) {
                     var i = 1;
                     for (; data[i];) {
                         var presentation = data[i][data[0].indexOf("PRESENTATION")].split("|");
+                        var requiredField = "";
+                        if (data[i][data[0].indexOf("REQUIRED")] == "1") {
+                            requiredField = " required='true'";
+                        }
                         usefulContent += "<p>" + data[i][data[0].indexOf("NAME")];
                         switch (data[i][data[0].indexOf("TYP")]) {
                             case 'textfield':
-                                usefulContent += "</p><input type='text' style='width:100%' id='" + data[i][data[0].indexOf("ID")] + "' />";
+                                usefulContent += " (máximo de " + presentation[1] + "caracteres)</p><input item='" + data[i][data[0].indexOf("ID")] + "' type='text' style='width:100%' maxlength='" + presentation[1] + "'" + requiredField + " id='" + data[i][data[0].indexOf("ID")] + "' />";
                                 break;
                             case 'numeric':
-                                usefulContent += " (" + presentation[0] + "-" + presentation[1] + ")</p><input type='number' style='width:100%' id='" + data[i][data[0].indexOf("ID")] + "' />";
+                                usefulContent += " (" + presentation[0] + "-" + presentation[1] + ")</p><input item='" + data[i][data[0].indexOf("ID")] + "' min='" + presentation[0] + "' max='" + presentation[1] + "'" +  requiredField + " type='number' style='width:100%' id='" + data[i][data[0].indexOf("ID")] + "' />";
                                 break;
                             case 'multichoice':
                                 var j = 0;
-                                usefulContent += "</p>";
+                                usefulContent += "</p><div" + requiredField + ">";
                                 for (j; j < presentation.length; j++) {
                                     if (presentation[j].indexOf("r>") == 0) {
                                         presentation[j] = presentation[j].substr(presentation[j].lastIndexOf(">") + 1);
@@ -201,11 +300,12 @@ function buildSurvey(surveyFile_) {
                                     }
                                     usefulContent += "<p><input type='radio' name='" + data[i][data[0].indexOf("ID")] + "' />" + presentation[j] + "</p>";
                                 }
+                                usefulContent += "</div>";
                                 break;
                             case 'info':
                                 break;
                             default:
-                                $("#content").html("Esta enquete contém tipos de alternativas não suportadas pelo Moodle.");
+                                $("#content").html("Esta enquete contém tipos de alternativas não suportadas pela aplicação.");
                                 return;
                         }
                         i++;
